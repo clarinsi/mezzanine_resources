6a3cdaf1e2385948868c2626cc54d958c4565ee2
Peter Rupnik <peter.rupnik@ijs.si>
2024-05-30 15:54:51 +0200

    Finished with the stats for paper

--- doone.py
+++ doone.py
@@ -2,8 +2,8 @@ try:
     infile = snakemake.input[0]
     outfile = snakemake.output[0]
 except:
-    infile = "SST/SPOG-Gos164-JRtvslpovp-rd0904201545.xml"
-    outfile = "snakedir/SST/SPOG-Gos164-JRtvslpovp-rd0904201545.csv"
+    infile = "SPOG/SPOG-Gos136-JRrajzcajp-rd0907171301.xml"
+    outfile = "snakedir/SPOG/SPOG-Gos136-JRrajzcajp-rd0907171301.csv"
 
 import pandas as pd
 import lxml.etree as ET
@@ -13,10 +13,30 @@ doc = ET.fromstring(Path(infile).read_bytes())
 # desc_type = doc.find(".//{*}desc[@type='topic']").text
 textdesc = doc.find(".//{*}textDesc").find(".//{*}domain").text
 
+excluded_w_ids = []
+if "SPOG/" in infile:
+    sstpath = Path(infile.replace("SPOG/", "SST/"))
+    if sstpath.exists():
+        print("SST will be filtered out")
+        sst = ET.fromstring(sstpath.read_bytes())
+        for w in sst.findall(".//{*}w"):
+            excluded_w_ids.append(w.get("{http://www.w3.org/XML/1998/namespace}id"))
+        print(f"Found {len(excluded_w_ids)} to filter out.")
 
-us = doc.findall(".//{*}u")
+us = list(doc.findall(".//{*}u"))
 whos = [u.get("who", "").replace("#", "") for u in us]
-num_ws = [len(list(u.findall(".//{*}w"))) for u in us]
+num_ws = [
+    len(
+        [
+            w
+            for w in u.findall(".//{*}w")
+            if (w.get("{http://www.w3.org/XML/1998/namespace}id") not in excluded_w_ids)
+            and (len(list(w.iter())) == 1)
+        ]
+        # + [w for w in u.findall(".//{*}pc")]
+    )
+    for u in us
+]
 
 df = pd.DataFrame(dict(who=whos, numw=num_ws)).groupby("who").numw.sum().reset_index()
 df["file"] = str(Path(infile).name)
--- gather.py
+++ gather.py
@@ -12,38 +12,56 @@ import pandas as pd
 import lxml.etree as ET
 from pathlib import Path
 
+
 files = list(Path(infile).glob("*.csv"))
-df = pd.concat([pd.read_csv(i) for i in files])
-df["speech"] = (
-    df.file.str.replace(".xml", "").str.replace("SPOG-", "").str.replace("Iriss-", "")
+df = pd.concat([pd.read_csv(i) for i in files]).fillna("-")
+if "iriss" in infile:
+    df["speech"] = (
+        df.file.str.replace(".xml", "")
+        .str.replace("SPOG-", "")
+        .str.replace("Iriss-", "")
+        .str.strip()
+    )
+else:
+    df["speech"] = (
+        df.file.str.replace(".xml", "")
+        .str.replace("SPOG-", "")
+        .str.replace("Iriss-", "")
+        .str.strip()
+        .apply(lambda s: s.split("-")[0])
+    )
+speakers = (
+    pd.read_csv("Gos.TEI/Gos-speakers.tsv", sep="\t")
+    .fillna("-")
+    .astype(str)
+    .reset_index(drop=True)
 )
-speakers = pd.read_csv("Gos.TEI/Gos-speakers.tsv", sep="\t").dropna()
 speeches = (
-    pd.read_csv("Gos.TEI/Gos-speeches.tsv", sep="\t")
-    .dropna()
-    .reset_index()
-    .rename(columns={"index": "speech"})
-)
-speeches["speech"] = speeches.speech.str.replace("Artur-", "")
+    pd.read_csv("Gos.TEI/Gos-speeches.tsv", sep="\t", index_col=False)
+    .fillna("-")
+    .reset_index(drop=True)
+).astype(str)
+speeches["speech"] = speeches["TEXT-ID"].str.replace("Artur-", "").str.strip()
+
+
 df = df.merge(
-    speakers["PRS-ID PERM-RESD CHILD-RESD SEX AGE SUBCORPUS".split()],
+    speakers["PRS-ID PERM-RESD CHILD-RESD SEX AGE".split()],
     left_on="who",
     right_on="PRS-ID",
     how="left",
 )
 df = df.merge(
-    speeches["speech".split()],
+    speeches["speech DOMAIN CHANNEL SUBCORPUS TYPE".split()],
     left_on="speech",
     right_on="speech",
     how="left",
 )
 c1 = df["PERM-RESD"].str.strip() == "-"
 df.loc[c1, "PERM-RESD"] = df["CHILD-RESD"][c1]
-c2 = df["CHILD-RESD"].str.strip() == "-"
-df.loc[c2, "CHILD-RESD"] = df["PERM-RESD"][c2]
+# c2 = df["CHILD-RESD"].str.strip() == "-"
+# df.loc[c2, "CHILD-RESD"] = df["PERM-RESD"][c2]
 df = df.drop_duplicates().reset_index(drop=True)
 df.to_csv(outfile, index=False)
-2 + 2
 
 
 from jinja2 import Environment, FileSystemLoader
@@ -51,7 +69,7 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader("."))
 template = env.get_template(name=templatepath)
 tip_diskurza = (
-    df.groupby(["SUBCORPUS", "textdesc"])
+    df.groupby(["SUBCORPUS", "DOMAIN", "CHANNEL", "TYPE"])
     .agg({"file": "nunique", "numw": "sum"})
     .rename(columns={"numw": "Število besed", "file": "Število posnetkov"})
 )
